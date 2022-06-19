fc4ca724b943a22a3942bfdb194381f11a2b6605
Kartikay <kartikayjoshiprivate@gmail.com>
2022-06-20 04:06:47 +0530

    EC2 script

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -4,7 +4,7 @@
 #2. Take that instance Ip & register in DNS
 
 if [ -z "$1" ]; then
-  echo "\e[1,31mInput is missing\e[0m"
+  echo -e "\e[1,31mInput is missing\e[0m"
   exit 1
 fi
 
@@ -13,6 +13,7 @@ COMPONENT=$1
 
 TEMP_ID="lt-0583d5443e6c153cb"
 TEMP_VER=1
+ZONE_ID=Z03342953N1E33YTO44DC
 
 
 # Check if instance is already there
@@ -20,12 +21,18 @@ TEMP_VER=1
 aws ec2 describe-instances --filters "Name=tag:Name,Values=COMPONENT" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &> /dev/null
 
 if [ $? -eq -0 ]; then
-   echo"\e[1,33mInstance is already there\e[0m"
-   exit
+   echo -e "\e[1,33mInstance is already there\e[0m"
+else
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=COMPONENT}]" |jq
 fi
 
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=COMPONENT}]" |jq
+IPADDRESS=$( aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null )
+
+sed -e "s/IPADDRESS/${IPADDRESS}/" -E "s/COMPONENT/${COMPONENT}" record. json >/tmp/record.json
+
+
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 
 #Update the DNS Record
 
