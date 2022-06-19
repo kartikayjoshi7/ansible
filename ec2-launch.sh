572ec004341d36f008b9a3b0e286004f77a08e46
Kartikay <kartikayjoshiprivate@gmail.com>
2022-06-20 02:19:07 +0530

    EC2 script

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -9,6 +9,7 @@ TEMP_ID="lt-0583d5443e6c153cb"
 TEMP_VER=1
 
 
+
 # Check if instance is already there
 aws run-instances --launch-template LaunchTemplateId=$(TEMP_ID), Version=$(TEMP_VER) --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" |jq
 
