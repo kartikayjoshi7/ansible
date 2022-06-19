31ccb6438f37047cdc3f51796b9acb7fdf444952
Kartikay <kartikayjoshiprivate@gmail.com>
2022-06-20 04:17:52 +0530

    EC2 script

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -4,7 +4,7 @@
 #2. Take that instance Ip & register in DNS
 
 if [ -z "$1" ]; then
-  echo -e "\e[1,31mInput is missing\e[0m"
+  echo -e "\e[1;31mInput is missing\e[0m"
   exit 1
 fi
 
@@ -18,12 +18,12 @@ ZONE_ID=Z03342953N1E33YTO44DC
 
 # Check if instance is already there
 
-aws ec2 describe-instances --filters "Name=tag:Name,Values=COMPONENT" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &> /dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &> /dev/null
 
 if [ $? -eq -0 ]; then
-   echo -e "\e[1,33mInstance is already there\e[0m"
+   echo -e "\e[1;33mInstance is already there\e[0m"
 else
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=COMPONENT}]" |jq
+  aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" |jq
 fi
 
 
