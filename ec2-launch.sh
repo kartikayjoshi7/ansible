1640836eda9d9a9ca9c6fc79b967b241e46157f4
Kartikay <kartikayjoshiprivate@gmail.com>
2022-06-20 04:24:05 +0530

    EC2 script

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -23,18 +23,20 @@ aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .R
 if [ $? -eq -0 ]; then
    echo -e "\e[1;33mInstance is already there\e[0m"
 else
-  aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" |jq
+  aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 fi
 
 
 IPADDRESS=$( aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null )
 
-sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}" record. json >/tmp/record.json
 
+#Update the DNS Record
+
+sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}" record.json >/tmp/record.json
 
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 
-#Update the DNS Record
+
 
 
 
