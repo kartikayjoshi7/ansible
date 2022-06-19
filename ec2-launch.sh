21cbf39d2e65d12039c9bbd38271b8672f9c13b5
Kartikay <kartikayjoshiprivate@gmail.com>
2022-06-20 02:16:16 +0530

    EC2 script

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -5,5 +5,12 @@
 
 
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specification "ResourceType=spot-instances-request, Tags=[{Key=Name,Value=frontend}]"
+TEMP_ID="lt-0583d5443e6c153cb"
+TEMP_VER=1
+
+
+# Check if instance is already there
+aws run-instances --launch-template LaunchTemplateId=$(TEMP_ID), Version=TEMP_VER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" |jq
+
+#Update the DNS Record
 
