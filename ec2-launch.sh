f8e1c7cb48f5eb91cb12fbe119a68290e4fecb85
Kartikay <kartikayjoshiprivate@gmail.com>
2022-06-20 02:16:40 +0530

    EC2 script

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -10,7 +10,7 @@ TEMP_VER=1
 
 
 # Check if instance is already there
-aws run-instances --launch-template LaunchTemplateId=$(TEMP_ID), Version=TEMP_VER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" |jq
+aws run-instances --launch-template LaunchTemplateId=$(TEMP_ID), Version=$(TEMP_VER) --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" |jq
 
 #Update the DNS Record
 
